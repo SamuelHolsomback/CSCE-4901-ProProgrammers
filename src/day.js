2fe846304bf6896632c4f7ee1564af1855459ca6
SamuelHolsomback <86018158+SamuelHolsomback@users.noreply.github.com>
2021-11-23 22:57:41 -0600

    Add files via upload
    Here's the bathroom view. The rendering is done in the index file, while the day component with the two bars is done in day.js. Both of those files, including index.css are required to render the project.

---
+++ src/day.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+
+class DayComponent extends Component {
+    constructor(){
+        super()
+        this.state = {
+          
+        }
+      }
+      render(){
+    
+        return ( 
+            <div>
+            <div class="TypeTest">{this.props.dayValue}</div>
+            <div class="TypeTest"> 
+              <div class="SolidTest">Solid </div>
+              <progress id="health" value={this.props.solidTimes} max="12"></progress>
+              <div class="SolidTest"> </div>
+              <div class="SolidTest">{this.props.solidTimes}</div>
+            </div>
+            <div class="TypeTest"> 
+              <div class="LiquidTest">Liquid</div>
+              <progress id="health" value={this.props.liquidTimes} max="12"></progress>
+              <div class="SolidTest">{this.props.liquidTimes}</div>
+            </div>
+            </div>
+      
+        )
+      }
+}
+export default DayComponent;
