3448347e6dca74218bb11b3b1b658d4b04195507
TheAstralShade <81994529+TheAstralShade@users.noreply.github.com>
2021-11-26 18:39:36 -0600

    Added requests for bathroom backend

--- index.js
+++ index.js
@@ -27,6 +27,46 @@ app.post('/register', (req, res)=> {
     });
 })
 
+app.post('/get-bathroom', (req, res)=> {
+    const baby = req.body.babyName;
+
+    db.query("SELECT * FROM restroom WHERE Baby = ?",
+    [baby],
+    (err, result) => {
+        if (result.length > 0) {
+            res.send(result)
+        }
+        else {
+            res.send({message: "No prior log entries!"})
+        }
+    });
+})
+
+app.post('/bathroom', (req, res)=> {
+
+    const mondaySValue = req.body.mondaySolidSet;
+    const tuesdaySValue = req.body.tuesdaySolidSet;
+    const wednesdaySValue = req.body.wednesdaySolidSet;
+    const thursdaySValue = req.body.thursdaySolidSet;
+    const fridaySValue = req.body.fridaySolidSet;
+    const saturdaySValue = req.body.saturdaySolidSet;
+    const sundaySValue = req.body.sundaySolidSet;
+    const mondayLValue = req.body.mondayLiquidSet;
+    const tuesdayLValue = req.body.tuesdayLiquidSet;
+    const wednesdayLValue = req.body.wednesdayLiquidSet;
+    const thursdayLValue = req.body.thursdayLiquidSet;
+    const fridayLValue = req.body.fridayLiquidSet;
+    const saturdayLValue = req.body.saturdayLiquidSet;
+    const sundayLValue = req.body.sundayLiquidSet;
+    //console.log(value)
+
+    db.query("UPDATE restroom SET Monday_Solid = ?, Tuesday_Solid = ?, Wednesday_Solid = ?, Thursday_Solid = ?, Friday_Solid = ?, Saturday_Solid = ?, Sunday_Solid = ?, Monday_Liquid = ?, Tuesday_Liquid = ?, Wednesday_Liquid = ?, Thursday_Liquid = ?, Friday_Liquid = ?, Saturday_Liquid = ?, Sunday_Liquid = ? WHERE Baby = 'Test'", 
+    [mondaySValue, tuesdaySValue, wednesdaySValue, thursdaySValue, fridaySValue, saturdaySValue, sundaySValue, mondayLValue, tuesdayLValue, wednesdayLValue, thursdayLValue, fridayLValue, saturdayLValue, sundayLValue], 
+    (err, result) => {
+        console.log(err);
+    });
+})
+
 app.post('/login', (req, res) => {
     const username = req.body.username;
     const password = req.body.password;
